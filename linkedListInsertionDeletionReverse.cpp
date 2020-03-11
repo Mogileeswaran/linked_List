47521bfbbe358ff8d7a948951e16997858b8ab4a
Mogileeswaran_M <53137599+Mogileeswaran@users.noreply.github.com>
2020-03-12 00:08:59 +0530

    Update and rename linkedListInsertionDeletion.cpp to linkedListInsertionDeletionReverse.cpp

--- linkedListInsertionDeletionReverse.cpp
+++ linkedListInsertionDeletionReverse.cpp
@@ -1,5 +1,5 @@
 
-///////////////////////       Linked List Insertion & Deletion      //////////////////////////
+///////////////////////       Linked List        //////////////////////////
 #include <iostream>
 using namespace std;
 
@@ -42,24 +42,41 @@ void deleteNode(int pos){
     }
     for(int i=1;i<pos-1;i++){
         temp1=temp1->next;
+       // cout<<i;
     }
     class Node* temp2=temp1->next;
     temp1->next=temp2->next;
     free(temp2);
+}
+void reverseList(){
+    class Node *current, *prev , *next;
+    current = head;
+    prev= nullptr;
+    while(current!=nullptr){
+        next=current->next;
+        current->next=prev;
+        prev=current;
+        current=next;
+    }
+    head=prev;
 }
 int main(){
     head=nullptr;
     Insert(1,1);
     Insert(2,2);
     Insert(3,3);
-    Insert(4,1);
-    Insert(7,2);
+    Insert(4,4);
+    Insert(7,1);
     print();
-    deleteNode(4);
+    deleteNode(5);
+    print();
+    reverseList();
     print();
     return 0;
 }
+
 /*
-4 7 1 2 3
-4 7 1 3
+7 1 2 3 4
+7 1 2 3
+3 2 1 7
 */
